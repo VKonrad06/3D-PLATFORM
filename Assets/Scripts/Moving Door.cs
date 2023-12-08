a4acd08bca4d1d5738deb7b9cbb9e46e9c4f9165
VKonrad06 <vkonrad2023@elam.co.uk>
2023-12-08 12:44:07 +0000

    ydjytju

---
+++ Assets/Scripts/Moving Door.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingDoor : MonoBehaviour
+{
+    public bool isOpen = false;
+
+    public Transform door;
+    public Transform startingPoint;
+    public Transform endingPoint;
+
+    private void Start()
+    {
+        door.position = startingPoint.position;
+    }
+
+    private void Update()
+    {
+        if (isOpen == true)
+        {
+            door.position = Vector3.MoveTowards(door.position, endingPoint.position, Time.deltaTime);
+        }
+    }
+}
